b22cf48f5670916ccf5697d3c267775f3a5225ec
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-02-08 20:46:59 +0000

    first commit

---
+++ models/park.js
@@ -0,0 +1,89 @@
+const Park = function (name, ticketPrice){
+    this.name = name;
+    this.ticketPrice = ticketPrice;
+    this.dinoCollection = [];
+    this.dinoDiets = {
+        'carnivore' : 0,
+        'herbivore' : 0,
+        'omnivore' : 0
+    };
+};
+
+Park.prototype.dinoCount = function(){
+    return this.dinoCollection.length;
+};
+
+Park.prototype.addDinosaur = function(newDino){
+    this.dinoCollection.push(newDino);
+};
+
+Park.prototype.removeDino = function(){
+    this.dinoCollection.pop();
+};
+
+Park.prototype.removeBySpecies = function(species){
+    let newCollection = [];
+    for (let dinosaur of this.dinoCollection){
+        if (dinosaur.species != species){
+            newCollection.push(dinosaur);
+        };
+    };
+    this.dinoCollection = newCollection;
+};
+
+Park.prototype.mostPopularDino = function(){
+    let visitorCount = 0;
+    for (let dinosaur of this.dinoCollection){
+        if (dinosaur.guestsAttractedPerDay > visitorCount){
+            visitorCount = dinosaur.guestsAttractedPerDay
+        };
+    };
+
+    let popularDino;
+    for (let dinosaur of this.dinoCollection){
+        if (dinosaur.guestsAttractedPerDay === visitorCount){
+            popularDino = dinosaur;
+        }
+    }
+    return popularDino;
+}
+
+Park.prototype.collectSpecies = function(species){
+    const speciesCollection = [];
+    for (let dinosaur of this.dinoCollection){
+        if (dinosaur.species === species){
+            speciesCollection.push(dinosaur);
+        };
+    };
+    return speciesCollection;
+};
+
+Park.prototype.totalDailyVisitors = function(){
+    let count = 0
+    for (let dinosaur of this.dinoCollection){
+        count += dinosaur.guestsAttractedPerDay;
+    };
+    return count;
+};
+
+Park.prototype.annualVisitorCount = function(){
+    let annualVisitors = this.totalDailyVisitors() * 365;
+    return annualVisitors;
+};
+
+Park.prototype.calculateAnnualRevenue = function(){
+    let annualRevenue = this.annualVisitorCount() * this.ticketPrice;
+    return annualRevenue;
+};
+
+Park.prototype.sortByDiet = function(){
+    for (var dinosaur in this.dinoCollection){
+        if (dinosaur.diet === this.dinoDiets.){
+            this.dinoDiets. += 1
+        };
+    };
+};
+
+// Ask Steve why const count didn't work but let did.
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,132 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  let park;
+
+  beforeEach(function () {
+    dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
+    dinosaur2 = new Dinosaur('velociraptor', 'carnivore', 25);
+    dinosaur3 = new Dinosaur('stegosaurus', 'herbivore', 20);
+    dinosaur4 = new Dinosaur('compsognathus', 'carnivore', 10);
+    dinosaur5 = new Dinosaur('velociraptor', 'carnivore', 30);
+    park = new Park('Dino-Mite Park', 10);
+  })
+
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Dino-Mite Park');
+  });
+
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 10);
+  });
+
+  it('should have a collection of dinosaurs', function(){
+    const count1 = park.dinoCount();
+    assert.strictEqual(count1, 0);
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    const count2 = park.dinoCount();
+    assert.strictEqual(count2, 4);
+  });
+
+
+  it('should be able to add a dinosaur to its collection', function(){
+    const newDino = new Dinosaur('dilophosaurus', 'carnivore', 20);
+    park.addDinosaur(newDino);
+    const actual = park.dinoCount();
+    assert.strictEqual(actual, 1);
+  });
+
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.removeDino();
+    const actual = park.dinoCount();
+    assert.strictEqual(actual, 3);
+  });
+
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    const actual = park.mostPopularDino();
+    const expected = dinosaur1
+    assert.strictEqual(actual, expected)
+  });
+
+  it('should be able to find all dinosaurs of a particular species', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    const speciesList = park.collectSpecies('velociraptor');
+    const actual = speciesList.length;
+    assert.strictEqual(actual, 2);
+  });
+
+  it('should be able to calculate the total number of visitors per day', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    const actual = park.totalDailyVisitors();
+    assert.strictEqual(actual, 135);
+  });
+
+  it('should be able to calculate the total number of visitors per year', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    const actual = park.annualVisitorCount();
+    assert.strictEqual(actual, 49275);
+  });
+
+  it('should be able to calculate total revenue for one year', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    const actual = park.calculateAnnualRevenue();
+    assert.strictEqual(actual, 492750);
+  });
+
+  it('should be able to remove a dinosaur by its species', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    park.removeBySpecies('velociraptor');
+    const actual = park.dinoCount();
+    assert.strictEqual(actual, 3);
+  });
+
+  it('should be able to populate an object based on diet', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    park.addDinosaur(dinosaur4);
+    park.addDinosaur(dinosaur5);
+    park.sortByDiet();
+    const actual = park.dinoDiets;
+    const expected = {};
+    assert.strictEqual(actual, expected);
+  })
+
+});
